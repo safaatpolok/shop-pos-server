5a94bcde005aa473cd67b0e79119d1d27938986f
anzumpolok <mdsafaatanzumpolok@gmail.com>
2026-04-08 23:55:44 +0600

    user update

--- src/main/java/com/shop/controller/UserController.java
+++ src/main/java/com/shop/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "https://shop-pos-client.vercel.app")
 @RestController
 
 @RequiredArgsConstructor
